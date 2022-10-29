8e0e15f7aa4e6410d2d8e7ced370117a5a546cea
Shawn Catanzarite <me@shawncatz.com>
2022-10-28 21:19:05 -0700

    disable downloads cron

--- app/cron.go
+++ app/cron.go
@@ -1,18 +1,17 @@
 package app
 
 import (
-	"github.com/pkg/errors"
 	"github.com/robfig/cron/v3"
 )
 
 func (s *Server) Cron() error {
 	c := cron.New(cron.WithSeconds())
-	if ConfigInstance().Cron {
-		// every 30 seconds DownloadsProcess
-		if _, err := c.AddFunc("*/30 * * * * *", s.DownloadsProcess); err != nil {
-			return errors.Wrap(err, "adding cron function")
-		}
-	}
+	//if ConfigInstance().Cron {
+	//	// every 30 seconds DownloadsProcess
+	//	if _, err := c.AddFunc("*/30 * * * * *", s.DownloadsProcess); err != nil {
+	//		return errors.Wrap(err, "adding cron function")
+	//	}
+	//}
 
 	go func() {
 		s.Log.Info("starting cron...")
