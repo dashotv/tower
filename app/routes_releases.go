414b24822ea15688b14b788846fc4d89478e61ca
Shawn Catanzarite <me@shawncatz.com>
2022-11-03 21:14:55 -0700

    add releases show

--- app/routes_releases.go
+++ app/routes_releases.go
@@ -34,7 +34,14 @@ func ReleasesCreate(c *gin.Context) {
 }
 
 func ReleasesShow(c *gin.Context, id string) {
-	c.JSON(http.StatusOK, gin.H{"error": false})
+	result := &Release{}
+	err := App().DB.Release.Find(id, result)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
 }
 
 func ReleasesUpdate(c *gin.Context, id string) {
