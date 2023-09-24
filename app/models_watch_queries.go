736748439b88c220ae5e976730c2f600c80e501a
Shawn Catanzarite <me@shawncatz.com>
2023-09-23 19:45:56 -0700

    more lint fixes

--- app/models_watch_queries.go
+++ app/models_watch_queries.go
@@ -5,10 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 func (c *Connector) MediumWatched(id primitive.ObjectID) bool {
 	// TODO: add user name to config
 	watches, _ := App().DB.Watch.Query().Where("medium_id", id).Where("username", "xenonsoul").Run()
-
-	if len(watches) > 0 {
-		return true
-	}
-
-	return false
+	return len(watches) > 0
 }
