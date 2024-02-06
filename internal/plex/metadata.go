ebef39ccc4b95f1efad0d55e1ec5d14d8f45fe98
Shawn Catanzarite <me@shawncatz.com>
2024-02-06 14:01:12 -0800

    move player-related into separate file

--- internal/plex/metadata.go
+++ internal/plex/metadata.go
@@ -1,9 +1,6 @@
 package plex
 
 import (
-	"fmt"
-	"net/url"
-
 	"github.com/pkg/errors"
 )
 
@@ -84,72 +81,4 @@ func (p *Client) GetSeriesEpisodesUnwatched(key string) (*PlexLeavesMetadata, er
 		}
 	}
 	return nil, nil
-}
-func (p *Client) Play(player, ratingKey string) error {
-	queue, err := p.playCreateQueue(ratingKey)
-	if err != nil {
-		return errors.Wrap(err, "failed to create queue")
-	}
-	if queue == nil {
-		return errors.New("failed to create queue")
-	}
-
-	params := url.Values{}
-	params.Set("protocol", "http")
-	params.Set("address", "10.0.4.61")
-	params.Set("port", "32400")
-	params.Set("offset", "0")
-	params.Set("commandID", "1")
-	params.Set("machineIdentifier", p.MachineIdentifier)
-	params.Set("type", "video")
-	params.Set("containerKey", fmt.Sprintf("/playQueues/%d", queue.MediaContainer.ID))
-	params.Set("key", "/library/metadata/"+ratingKey)
-	resp, err := p._server().SetHeaders(p.Headers).
-		SetHeader("X-Plex-Target-Client-Identifier", player).
-		SetQueryParamsFromValues(params).
-		Get("/player/playback/playMedia")
-	if err != nil {
-		return errors.Wrap(err, "failed to make request")
-	}
-	if !resp.IsSuccess() {
-		return errors.Errorf("failed to play: %s", resp.Status())
-	}
-	fmt.Printf("play: %s\n", resp.String())
-	return nil
-}
-
-type PlexQueue struct {
-	MediaContainer struct {
-		Size            int64  `json:"size"`
-		Identifier      string `json:"identifier"`
-		MediaTagPrefix  string `json:"mediaTagPrefix"`
-		MediaTagVersion int64  `json:"mediaTagVersion"`
-		Shuffled        bool   `json:"playQueueShuffled"`
-		Source          string `json:"playQueueSourceURI"`
-		Version         int64  `json:"playQueueVersion"`
-		ID              int64  `json:"playQueueID"`
-	}
-}
-
-func (p *Client) playCreateQueue(ratingKey string) (*PlexQueue, error) {
-	q := &PlexQueue{}
-
-	params := url.Values{}
-	params.Set("type", "video")
-	params.Set("shuffle", "0")
-	params.Set("repeat", "0")
-	params.Set("continuous", "0")
-	params.Set("own", "1")
-	params.Set("uri", fmt.Sprintf("server://%s/com.plexapp.plugins.library/library/metadata/%s", p.MachineIdentifier, ratingKey))
-
-	resp, err := p._server().SetResult(q).SetHeaders(p.Headers).SetQueryParamsFromValues(params).Post("/playQueues")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to make request")
-	}
-	if !resp.IsSuccess() {
-		return nil, errors.Errorf("failed to play: %s", resp.Status())
-	}
-	fmt.Printf("queue: %s\n", resp.String())
-	fmt.Printf("queue: %+v\n", q)
-	return q, nil
 }
---
+++ internal/plex/player.go
@@ -0,0 +1,80 @@
+package plex
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/pkg/errors"
+)
+
+func (p *Client) Play(player, ratingKey string) error {
+	queue, err := p.playCreateQueue(ratingKey)
+	if err != nil {
+		return errors.Wrap(err, "failed to create queue")
+	}
+	if queue == nil {
+		return errors.New("failed to create queue")
+	}
+
+	return p.playQueue(queue.MediaContainer.ID, player, ratingKey)
+}
+
+type PlexQueue struct {
+	MediaContainer struct {
+		Size            int64  `json:"size"`
+		Identifier      string `json:"identifier"`
+		MediaTagPrefix  string `json:"mediaTagPrefix"`
+		MediaTagVersion int64  `json:"mediaTagVersion"`
+		Shuffled        bool   `json:"playQueueShuffled"`
+		Source          string `json:"playQueueSourceURI"`
+		Version         int64  `json:"playQueueVersion"`
+		ID              int64  `json:"playQueueID"`
+	}
+}
+
+func (p *Client) playQueue(queueID int64, ratingKey, player string) error {
+	params := url.Values{}
+	params.Set("protocol", "http")
+	params.Set("address", "10.0.4.61")
+	params.Set("port", "32400")
+	params.Set("offset", "0")
+	params.Set("commandID", "1")
+	params.Set("machineIdentifier", p.MachineIdentifier)
+	params.Set("type", "video")
+	params.Set("containerKey", fmt.Sprintf("/playQueues/%d", queueID))
+	params.Set("key", "/library/metadata/"+ratingKey)
+	resp, err := p._server().SetHeaders(p.Headers).
+		SetHeader("X-Plex-Target-Client-Identifier", player).
+		SetQueryParamsFromValues(params).
+		Get("/player/playback/playMedia")
+	if err != nil {
+		return errors.Wrap(err, "failed to make request")
+	}
+	if !resp.IsSuccess() {
+		return errors.Errorf("failed to play: %s", resp.Status())
+	}
+	return nil
+}
+
+func (p *Client) playCreateQueue(ratingKey string) (*PlexQueue, error) {
+	q := &PlexQueue{}
+
+	params := url.Values{}
+	params.Set("type", "video")
+	params.Set("shuffle", "0")
+	params.Set("repeat", "0")
+	params.Set("continuous", "0")
+	params.Set("own", "1")
+	params.Set("uri", fmt.Sprintf("server://%s/com.plexapp.plugins.library/library/metadata/%s", p.MachineIdentifier, ratingKey))
+
+	resp, err := p._server().SetResult(q).SetHeaders(p.Headers).SetQueryParamsFromValues(params).Post("/playQueues")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make request")
+	}
+	if !resp.IsSuccess() {
+		return nil, errors.Errorf("failed to play: %s", resp.Status())
+	}
+	fmt.Printf("queue: %s\n", resp.String())
+	fmt.Printf("queue: %+v\n", q)
+	return q, nil
+}
