3c43315e3233492302b81fa1171aa52ee311be55
Shawn Catanzarite <me@shawncatz.com>
2024-03-09 02:59:09 -0800

    allow downloads to get created, add log and notice

--- app/events_runic_releases.go
+++ app/events_runic_releases.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"fmt"
+	"time"
+
 	runic "github.com/dashotv/runic/app"
 )
 
@@ -11,10 +14,17 @@ func onRunicReleases(a *Application, msg *runic.Release) error {
 		return err
 	}
 
+	// disable for now, because I want to see if the matching is working
+	// if !series.Active {
+	// 	return nil, nil
+	// }
+
 	episode, err := getEpisodeBySeries(series, msg.Season, msg.Episode)
 	if episode == nil {
 		return err
 	}
+
+	a.Log.Named("runic.releases").Warnf("found: %s S%02dE%02d", msg.Title, msg.Season, msg.Episode)
 
 	d := &Download{}
 	d.MediumId = episode.ID
@@ -22,6 +32,17 @@ func onRunicReleases(a *Application, msg *runic.Release) error {
 	d.Url = "metube://" + msg.Download
 
 	if err := a.DB.Download.Save(d); err != nil {
+		return err
+	}
+
+	notice := &EventNotices{
+		Event:   "Download Created",
+		Time:    time.Now().String(),
+		Class:   "runic",
+		Level:   "warn",
+		Message: fmt.Sprintf("found: %s S%02dE%02d", msg.Title, msg.Season, msg.Episode),
+	}
+	if err := a.Events.Send("tower.notices", notice); err != nil {
 		return err
 	}
 
@@ -34,9 +55,6 @@ func getSeriesBySearch(title string) (*Series, error) {
 		return nil, err
 	}
 	if len(list) != 1 {
-		return nil, nil
-	}
-	if !list[0].Active {
 		return nil, nil
 	}
 
