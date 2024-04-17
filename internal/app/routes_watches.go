6c753a8bc3535ab811fb8f87b8afab762d4fdf4a
Shawn Catanzarite <me@shawncatz.com>
2024-04-17 21:16:36 +0000

    delete watch by id and medium id

--- internal/app/routes_watches.go
+++ internal/app/routes_watches.go
@@ -1,10 +1,12 @@
 package app
 
 import (
+	"context"
 	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // GET /watches/
@@ -51,4 +53,34 @@ func (a *Application) WatchesCreate(c echo.Context, medium_id string, username s
 	}
 
 	return c.JSON(http.StatusOK, &Response{Error: false, Result: watch})
+}
+
+// DELETE /watches/:id
+func (a *Application) WatchesDelete(c echo.Context, id string) error {
+	w, err := app.DB.Watch.Get(id, &Watch{})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{Error: true, Message: "loading watch:" + err.Error()})
+	}
+	if w == nil {
+		return c.JSON(http.StatusInternalServerError, &Response{Error: true, Message: "watch not found"})
+	}
+
+	if err := app.DB.Watch.Delete(w); err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{Error: true, Message: "deleting watch:" + err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, &Response{Error: false, Message: "watch deleted"})
+}
+
+// DELETE /watches/medium
+func (a *Application) WatchesDeleteMedium(c echo.Context, medium_id string) error {
+	if medium_id == "" {
+		return c.JSON(http.StatusBadRequest, &Response{Error: true, Message: "medium_id is required"})
+	}
+
+	if _, err := app.DB.Watch.Collection.DeleteMany(context.Background(), bson.M{"medium_id": medium_id}); err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{Error: true, Message: "deleting watch:" + err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, &Response{Error: false, Message: "watch deleted"})
 }
