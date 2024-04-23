0480023adef7b5f8173a57d68873d7d7a1a13145
Shawn Catanzarite <me@shawncatz.com>
2024-04-23 02:06:00 +0000

    missing template attribute on destination templates

--- internal/app/models.gen.go
+++ internal/app/models.gen.go
@@ -269,7 +269,8 @@ type DestinationTemplate struct { // model
 	//ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	//CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	//UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
-	Name string `bson:"name" json:"name"`
+	Name     string `bson:"name" json:"name"`
+	Template string `bson:"template" json:"template"`
 }
 
 type Download struct { // model
