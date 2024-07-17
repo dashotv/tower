85feeb8940af0c6f77827019aa6d5072ed40d14d
Shawn Catanzarite <me@shawncatz.com>
2024-07-17 01:21:43 +0000

    use dir instead of dup code

--- internal/app/workers_paths.go
+++ internal/app/workers_paths.go
@@ -73,7 +73,7 @@ func (j *PathManage) Work(ctx context.Context, job *minion.Job[*PathManage]) err
 	}
 
 	dir := fmt.Sprintf("%s/%s", lib.Path, medium.Directory)
-	if !exists(fmt.Sprintf("%s/%s", lib.Path, medium.Directory)) {
+	if !exists(dir) {
 		medium.Broken = true
 		if err := app.DB.Medium.Save(medium); err != nil {
 			return fae.Wrap(err, "save medium")
